94f6c17775743c05019c011e4b0ffcabb9a90ade
Floris van Hees <s1145463@student.hsleiden.nl>
2024-04-09 06:45:08 +0200

    Refactored all code and modulated page adding

--- ipsen5_data_hound/Services/ValuesService.cs
+++ ipsen5_data_hound/Services/ValuesService.cs
@@ -2,113 +2,46 @@ namespace ipsen5_data_hound.Services
 {
     public class ValuesService
     {
-        public List<bool> leftValues1 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues1 { get; set; } = new List<bool> { false, false, false };
+        public int totalCases { get; set; } = 0;
+        public List<List<bool>> leftValues { get; set; }
+        public List<List<bool>> rightValues { get; set; }
 
-        public List<bool> leftValues2 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues2 { get; set; } = new List<bool> { false, false, false };
-
-        public List<bool> leftValues3 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues3 { get; set; } = new List<bool> { false, false, false };
-
-        public List<bool> leftValues4 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues4 { get; set; } = new List<bool> { false, false, false };
-
-        public List<bool> leftValues5 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues5 { get; set; } = new List<bool> { false, false, false };
-
-        public List<bool> leftValues6 { get; set; } = new List<bool> { false, false, false };
-        public List<bool> rightValues6 { get; set; } = new List<bool> { false, false, false };
-
+        public List<bool> showTable { get; set; }
+        public ValuesService()
+        {
+            
+            leftValues = new List<List<bool>>();
+            rightValues = new List<List<bool>>();
+            showTable = new List<bool>();
+            AddNewCase();
+            Console.WriteLine(leftValues+ "startup");
+        }
+        
+        
+        public void AddNewCase()
+        {
+            leftValues.Add(new List<bool> { false, false, false });
+            rightValues.Add(new List<bool> { false, false, false });
+            showTable.Add(true);
+            Console.WriteLine(leftValues.Count + "adding new case");
+            totalCases = leftValues.Count;
+        }
         public void ToggleLeftValue(int index, int listNumber)
         {
-            switch (listNumber)
-            {
-                case 1:
-                    leftValues1[index] = !leftValues1[index];
-                    break;
-                case 2:
-                    leftValues2[index] = !leftValues2[index];
-                    break;
-                case 3:
-                    leftValues3[index] = !leftValues3[index];
-                    break;
-                case 4:
-                    leftValues4[index] = !leftValues4[index];
-                    break;
-                case 5:
-                    leftValues5[index] = !leftValues5[index];
-                    break;
-                case 6:
-                    leftValues6[index] = !leftValues6[index];
-                    break;
-            }
+            leftValues[listNumber][index] = !leftValues[listNumber][index];
         }
 
         public void ToggleRightValue(int index, int listNumber)
         {
-            switch (listNumber)
-            {
-                case 1:
-                    rightValues1[index] = !rightValues1[index];
-                    break;
-                case 2:
-                    rightValues2[index] = !rightValues2[index];
-                    break;
-                case 3:
-                    rightValues3[index] = !rightValues3[index];
-                    break;
-                case 4:
-                    rightValues4[index] = !rightValues4[index];
-                    break;
-                case 5:
-                    rightValues5[index] = !rightValues5[index];
-                    break;
-                case 6:
-                    rightValues6[index] = !rightValues6[index];
-                    break;
-            }
+            rightValues[listNumber][index] = !rightValues[listNumber][index];   
         }
         public string DetermineColor(int listNumber)
         {
-            List<bool> leftValues;
-            List<bool> rightValues;
-
-            switch (listNumber)
-            {
-                case 1:
-                    leftValues = leftValues1;
-                    rightValues = rightValues1;
-                    break;
-                case 2:
-                    leftValues = leftValues2;
-                    rightValues = rightValues2;
-                    break;
-                case 3:
-                    leftValues = leftValues3;
-                    rightValues = rightValues3;
-                    break;
-                case 4:
-                    leftValues = leftValues4;
-                    rightValues = rightValues4;
-                    break;
-                case 5:
-                    leftValues = leftValues5;
-                    rightValues = rightValues5;
-                    break;
-                case 6:
-                    leftValues = leftValues6;
-                    rightValues = rightValues6;
-                    break;
-                default:
-                    throw new ArgumentException("Invalid list number");
-            }
-
-            if (leftValues.All(x => !x) && rightValues.All(x => !x))
+            if (leftValues[listNumber].All(x => !x) && rightValues[listNumber].All(x => !x))
             {
                 return "red";
             }
-            else if (leftValues.All(x => x) && rightValues.All(x => x))
+            else if (leftValues[listNumber].All(x => x) && rightValues[listNumber].All(x => x))
             {
                 return "green";
             }
@@ -117,5 +50,55 @@ namespace ipsen5_data_hound.Services
                 return "orange";
             }
         }
+        public void ToggleTable(int listNumber)
+        {
+            showTable[listNumber] = !showTable[listNumber];
+        }
+        
+        public int GetTotalCompletedCases()
+        {
+            int completedCases = 0;
+
+            for (int i = 0; i < totalCases; i++)
+            {
+                if (DetermineColor(i) == "green")
+                {
+                    completedCases++;
+                }
+            }
+
+            return completedCases;
+        }
+        
+        public int GetInActionCases()
+        {
+            int inActionCases = 0;
+
+            for (int i = 0; i < totalCases; i++)
+            {
+                if (DetermineColor(i) == "orange")
+                {
+                    inActionCases++;
+                }
+            }
+
+            return inActionCases;
+        }
+
+        public int GetNonActiveCases()
+        {
+            int nonActiveCases = 0;
+
+            for (int i = 0; i < totalCases; i++)
+            {
+                if (DetermineColor(i) == "red")
+                {
+                    nonActiveCases++;
+                }
+            }
+
+            return nonActiveCases;
+        }
+
     }
 }
